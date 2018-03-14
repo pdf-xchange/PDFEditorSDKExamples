f60c12c2c5e137c8c7f953c373835febb4c3d8c8
Dima <dima@tracker-software.com>
2018-03-14 13:00:22 +0200

    add file

---
+++ CSharp/OptimizeImagesCompression/OptimizeImagesCompression/GetTestFiles.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+
+namespace OptimizeImagesCompression
+{
+	class GetTestFiles
+	{
+		public static string FolderWithTestFilesPath { get; set; }
+		public static string[] GetAllFilesInFolder()
+		{
+			return Directory.GetFiles(FolderWithTestFilesPath);
+		}
+	}
+}
