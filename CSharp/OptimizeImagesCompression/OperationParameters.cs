ebd6cbedf620603267c650c53da8f221a643eb16
Dima <dima@tracker-software.com>
2018-04-30 09:58:26 +0300

    move file OptimizeImagesCompression

---
+++ CSharp/OptimizeImagesCompression/OperationParameters.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace OptimizeImagesCompression
+{
+	class OperationParameters
+	{
+		public string FilePath;
+		public string OutputFilePath;
+		public string CompMode;
+		public int Method;
+		public int Quality;
+		public string ErrCodes = "";
+		public long Time = 0;
+		public long OptimazedFileSize = 0;
+		public long OriginalFileSize = 0;
+	}
+}
