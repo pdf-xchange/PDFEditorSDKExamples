780673112aaa49fca23d92c9965f5fda7e039e5b
Dima Olijnyk <TFSHOST\dima>
2026-03-11 10:39:32 +0200

    review code

--- CSharp/CustomTool/MainForm.cs
+++ CSharp/CustomTool/MainForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace CustomTool
@@ -12,32 +12,43 @@ namespace CustomTool
         MI_Tool_Activate = 0,
         MI_Tool_Save = 2,
     }
+
     public partial class MainForm : Form
     {
         private readonly PagesMarkupTool MarkupTool;
+
         public MainForm()
         {
             InitializeComponent();
 
             MarkupTool = new PagesMarkupTool(this);
             pdfCtl.Inst.RegisterTool(MarkupTool);
-            menuForm.Items.Clear();
-            menuForm.Items.Add("Document");
-            menuForm.Items.Add("Tool");
 
-            var tmpItem = menuForm.Items[(int)MenuItems.MI_Document] as ToolStripMenuItem;
-            tmpItem?.DropDown.Items.Add(new ToolStripMenuItem("Open", null, this.btnOpen_Click));
-            tmpItem?.DropDown.Items.Add("-");
-            tmpItem?.DropDown.Items.Add(new ToolStripMenuItem("Close", null, this.btnCloseDoc_Click));
-            tmpItem.DropDown.Items[(int)MenuItems.MI_Doc_Close].Enabled = false;
-
-            tmpItem = menuForm.Items[(int)MenuItems.MI_Tool] as ToolStripMenuItem;
-            tmpItem?.DropDown.Items.Add(new ToolStripMenuItem("Activate", null, this.btnMarkupTool_CheckedChanged));
-            tmpItem.DropDown.Items[(int)MenuItems.MI_Tool_Activate].Enabled = false;
-            tmpItem?.DropDown.Items.Add("-");
-            tmpItem?.DropDown.Items.Add(new ToolStripMenuItem("Save selected", null, this.btnProcessSelRegions_Click));
-
+            InitializeMenu();
             OnCustomRegionsChanged();
+        }
+
+        private void InitializeMenu()
+        {
+            menuForm.Items.Clear();
+            menuForm.Items.Add(new ToolStripMenuItem("Document"));
+            menuForm.Items.Add(new ToolStripMenuItem("Tool"));
+
+            var docMenu = menuForm.Items[(int)MenuItems.MI_Document] as ToolStripMenuItem;
+            if (docMenu != null)
+            {
+                docMenu.DropDown.Items.Add(new ToolStripMenuItem("Open", null, btnOpen_Click));
+                docMenu.DropDown.Items.Add("-");
+                docMenu.DropDown.Items.Add(new ToolStripMenuItem("Close", null, btnCloseDoc_Click) { Enabled = false });
+            }
+
+            var toolMenu = menuForm.Items[(int)MenuItems.MI_Tool] as ToolStripMenuItem;
+            if (toolMenu != null)
+            {
+                toolMenu.DropDown.Items.Add(new ToolStripMenuItem("Activate", null, btnMarkupTool_CheckedChanged) { Enabled = false });
+                toolMenu.DropDown.Items.Add("-");
+                toolMenu.DropDown.Items.Add(new ToolStripMenuItem("Save selected", null, btnProcessSelRegions_Click));
+            }
         }
 
         private void btnOpen_Click(object sender, EventArgs e)
@@ -47,11 +58,14 @@ namespace CustomTool
                 pdfCtl.OpenDocWithDlg();
                 if (pdfCtl.Doc != null)
                 {
-                    (menuForm.Items[(int)MenuItems.MI_Document] as ToolStripMenuItem).DropDown.Items[(int)MenuItems.MI_Doc_Close].Enabled = true;
-                    (menuForm.Items[(int)MenuItems.MI_Tool] as ToolStripMenuItem).DropDown.Items[(int)MenuItems.MI_Tool_Activate].Enabled = true;
-                }                
+                    SetMenuItemEnabled(MenuItems.MI_Document, MenuItems.MI_Doc_Close, true);
+                    SetMenuItemEnabled(MenuItems.MI_Tool, MenuItems.MI_Tool_Activate, true);
+                }
             }
-            catch { }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening document: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnCloseDoc_Click(object sender, EventArgs e)
@@ -59,35 +73,65 @@ namespace CustomTool
             try
             {
                 pdfCtl.Doc?.Close();
-                (sender as ToolStripMenuItem).Enabled = false;
+                var closeMenuItem = sender as ToolStripMenuItem;
+                if (closeMenuItem != null)
+                    closeMenuItem.Enabled = false;
 
-                var item = (menuForm.Items[(int)MenuItems.MI_Tool] as ToolStripMenuItem);
-                ToolStripMenuItem toolStripMenuItem = (item.DropDownItems[(int)MenuItems.MI_Tool_Activate] as ToolStripMenuItem);
-                toolStripMenuItem.Checked = false;
-                toolStripMenuItem.Enabled = false;
+                SetMenuItemChecked(MenuItems.MI_Tool, MenuItems.MI_Tool_Activate, false);
+                SetMenuItemEnabled(MenuItems.MI_Tool, MenuItems.MI_Tool_Activate, false);
             }
-            catch { }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error closing document: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void ActivateMarkupTool() => pdfCtl.Doc?.ActivateTool(MarkupTool != null ? MarkupTool.ID : pdfCtl.Inst.Str2ID("tool.hand"));
+        private void ActivateMarkupTool() =>
+            pdfCtl.Doc?.ActivateTool(MarkupTool != null ? MarkupTool.ID : pdfCtl.Inst.Str2ID("tool.hand"));
 
-        private void DeactivateMarkupTool() => pdfCtl.Doc?.ActivateTool(pdfCtl.Inst.Str2ID("tool.hand"));
+        private void DeactivateMarkupTool() =>
+            pdfCtl.Doc?.ActivateTool(pdfCtl.Inst.Str2ID("tool.hand"));
 
-        private void btnProcessSelRegions_Click(object sender, EventArgs e) => MarkupTool.ProcessSelRegions();
+        private void btnProcessSelRegions_Click(object sender, EventArgs e) =>
+            MarkupTool.ProcessSelRegions();
 
         public void OnCustomRegionsChanged()
         {
-            var item = (menuForm.Items[(int)MenuItems.MI_Tool] as ToolStripMenuItem);
-            item.DropDownItems[(int)MenuItems.MI_Tool_Save].Enabled = MarkupTool.PageRegions.Count != 0;
+            SetMenuItemEnabled(MenuItems.MI_Tool, MenuItems.MI_Tool_Save, MarkupTool.PageRegions.Count != 0);
         }
+
         private void btnMarkupTool_CheckedChanged(object sender, EventArgs e)
         {
-            var send = (sender as ToolStripMenuItem);
+            var send = sender as ToolStripMenuItem;
+            if (send == null) return;
+
             send.Checked = !send.Checked;
             if (!send.Checked)
                 DeactivateMarkupTool();
             else
                 ActivateMarkupTool();
+        }
+
+        private void SetMenuItemEnabled(MenuItems parent, MenuItems child, bool enabled)
+        {
+            var parentMenu = menuForm.Items[(int)parent] as ToolStripMenuItem;
+            if (parentMenu != null && parentMenu.DropDown.Items.Count > (int)child)
+            {
+                var item = parentMenu.DropDown.Items[(int)child] as ToolStripMenuItem;
+                if (item != null)
+                    item.Enabled = enabled;
+            }
+        }
+
+        private void SetMenuItemChecked(MenuItems parent, MenuItems child, bool checkedState)
+        {
+            var parentMenu = menuForm.Items[(int)parent] as ToolStripMenuItem;
+            if (parentMenu != null && parentMenu.DropDown.Items.Count > (int)child)
+            {
+                var item = parentMenu.DropDown.Items[(int)child] as ToolStripMenuItem;
+                if (item != null)
+                    item.Checked = checkedState;
+            }
         }
     }
 }
